23a6956ccbfe8b5f33db4324532d3143d67dc65e
jrnelson90 <jrnelson90@gmail.com>
2016-06-21 20:08:47 -0600

    Added Prototype Start Screen
    Logo and Start button with slide animation

--- JQuerySudoku/jQSudoku.js
+++ JQuerySudoku/jQSudoku.js
@@ -6,6 +6,8 @@ var defaultGridLength = 9;
 
 $(document).ready(function () {
     gameView.getBrowserInfo();
+    gameView.drawStartScreen();
+    $("#startPage").css({"opacity": "1"});
     // Create 9x9 Grid
     gameView.drawGameGrid(defaultGridLength, defaultGridLength);
     if(document.body.offsetWidth < 600) {
@@ -42,6 +44,51 @@ function SudokuView() {
     var fontBold;
     var fontNote;
     var gridBlurred = false;
+    var startOpen;
+    var $startScreen;
+
+    this.drawStartScreen = function () {
+        startOpen = true;
+        // Create Start Page DIV
+        $("body").append('<div id=\"startPage\">');
+        $startScreen = $("#startPage");
+        $startScreen.css({"width": window.innerWidth + "px"});
+
+        $startScreen.append('<img src=\"logo.png\" id=\"startLogo\">');
+        $startScreen.append('<div id=\"startMenuCont\">');
+        $("#startMenuCont").append('<div id=\"startBtn\">');
+        $("#startBtn").html("START GAME");
+        $("#startBtn").click(function () {
+            //gameView.navTone();
+
+            // if(gameView.selectToggle() == true)
+            //     gameView.collapseSelect();
+            gameView.slideStartClose();
+
+            /*setTimeout(function() {
+                if (gameView.loaded() == false && gameModel.completed() == false) {
+                    if ($("#selText").innerHTML == "Difficulty") {
+                        $("#selText").html("Medium");
+                    }
+                    document.getElementById("puzzleDiffText").innerHTML = document.getElementById("selText").innerHTML;
+                    gameView.showLoading();
+                    setTimeout(function() {
+                        gameControl.loadSelectedPuzzle();
+                    }, 300);
+                }
+            },350);*/
+        });
+    };
+
+    this.slideStartClose = function() {
+        startOpen = false;
+        var currentStartWidth = parseInt($startScreen.css("width"));
+        console.log(currentStartWidth);
+        $startScreen.css({"left": (0 - currentStartWidth) + "px"});
+        setTimeout(function() {
+            $startScreen.css({"visibility": "hidden"});
+        },510);
+    };
 
     this.drawGameGrid = function (_rows, _columns) {
         var $gameGrid = $("#gameGrid");
@@ -57,8 +104,7 @@ function SudokuView() {
     this.resizeView = function() {
         var cellSize;
         var gridSize;
-        setViewHeight(window.innerHeight);
-        setViewWidth(window.innerWidth);
+        this.getBrowserInfo();
         // Define the CSS style of gameGrid to have a black border with curved corners and a white background
         if(this.getViewWidth() < this.getViewHeight()) {
             if (gameView.getIsMobile() && document.body.offsetWidth < 420){
@@ -96,6 +142,8 @@ function SudokuView() {
         else
             gameView.setIsMobile(false);
         console.log("isMobile = " + gameView.getIsMobile());
+        setViewHeight(window.innerHeight);
+        setViewWidth(window.innerWidth);
     };
 
     this.getCellSize = function () {
@@ -129,4 +177,48 @@ function SudokuView() {
     this.setIsMobile = function (_bool) {
         isMobile = _bool;
     };
+
+    this.selectToggle = function() {
+        return selectToggle;
+    };
+
+    this.expandSelect = function() {
+        if (gameView.getInputVisibility() == true) {
+            this.closeInputGrid();
+        }
+        var list = document.getElementById("selDropCont");
+        list.style.visibility = "visible";
+        list.style.display = "block";
+        list.style.height = "auto";
+        setTimeout(function(){
+            var options = document.getElementsByClassName("selOpt");
+            for (var i = 0; i < options.length; i++) {
+                options[i].style.display = "block";
+                options[i].style.visibility = "visible";
+                //options[i].style.height = "24px";
+            }
+        }, 50);
+        selectToggle = true;
+    };
+
+    this.collapseSelect = function() {
+        var options = document.getElementsByClassName("selOpt");
+        for (var i = 0; i < options.length; i++) {
+            options[i].style.visibility = "hidden";
+            options[i].style.display = "none";
+            //options[i].style.height = "0";
+        }
+        setTimeout(function(){
+            var list = document.getElementById("selDropCont");
+            list.style.height = "0";
+            list.style.display = "none";
+            list.style.visibility = "hidden";
+        }, 50);
+        selectToggle = false;
+    };
+
+    this.navTone = function() {
+        $("#navSound").play();
+    };
+
 }
